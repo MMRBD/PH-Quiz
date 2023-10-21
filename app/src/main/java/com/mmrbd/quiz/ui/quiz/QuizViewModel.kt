9966af18aa2da4e0a2902b104760014f3415f09f
Moklesur Rahman <mmrrahman508@gmail.com>
2023-10-21 19:10:11 +0600

    quiz viewModel added

---
+++ app/src/main/java/com/mmrbd/quiz/ui/quiz/QuizViewModel.kt
@@ -0,0 +1,54 @@
+package com.mmrbd.quiz.ui.quiz
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mmrbd.quiz.utils.network.Result
+import com.mmrbd.quiz.data.model.QuestionResponse
+import com.mmrbd.quiz.data.model.Question
+import com.mmrbd.quiz.data.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuizViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    val questionResponseSFlow = MutableSharedFlow<Result<QuestionResponse>>()
+    val questionFlow = MutableSharedFlow<Question>()
+    val questionCounterFlow = MutableStateFlow<Int>(0)
+
+    init {
+        getQuestions()
+    }
+
+    private fun getQuestions() {
+        viewModelScope.launch {
+            repository.geQuestions().collect {
+                questionResponseSFlow.emit(
+                    it
+                )
+            }
+        }
+    }
+
+    fun setQuestionX(question: Question, position: Int) {
+
+
+        viewModelScope.launch {
+            if (position != 0)
+                delay(2000)
+            questionFlow.emit(question)
+        }
+    }
+
+    fun incrementCounter(number: Int) {
+        viewModelScope.launch {
+            questionCounterFlow.emit(number)
+        }
+    }
+}
