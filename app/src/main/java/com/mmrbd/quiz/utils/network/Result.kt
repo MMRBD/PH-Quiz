21e6d232c379af66779d12f4172c73bcbaec17f1
Moklesur Rahman <mmrrahman508@gmail.com>
2023-10-21 19:17:24 +0600

    result response utils added

---
+++ app/src/main/java/com/mmrbd/quiz/utils/network/Result.kt
@@ -0,0 +1,16 @@
+package com.mmrbd.quiz.utils.network
+
+
+/**
+ * A wrapper for network request handling failing requests
+ * @param data
+ * @param error
+ * */
+sealed class Result<T>(
+    val data: T? = null,
+    val error: Failure? = null
+) {
+    class Success<T>(data: T) : Result<T>(data)
+    class Loading<T>(data: T? = null) : Result<T>(data)
+    class Error<T>(val throwable: Failure, data: T? = null) : Result<T>(data, throwable)
+}
